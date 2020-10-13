20220efda684b44a6911b880a888fe4ed9fa22be
nakajima123456789 <sou38608@gmail.com>
2020-10-14 04:30:42 +0900

    マップクラスコーディング、ラムダ式記述に変更

--- ブレイクアクセラレータR/ES Game Library/MyClass/C_MAP/C_MAP.cpp
+++ ブレイクアクセラレータR/ES Game Library/MyClass/C_MAP/C_MAP.cpp
@@ -2,24 +2,22 @@
 
 void C_MAP::Init()
 {
-	model = GraphicsDevice.CreateModelFromFile(_T("model3D//âºëfçﬁ//hashira_01.X"));
-	this->model->SetScale(transform.scale);
-	model_road = GraphicsDevice.CreateModelFromFile(_T("model3D//ìπòH//road_v01.X"));
-	this->model_road->SetScale(transform.scale);
+	auto&& ModelPrefarence = [this](MODEL& _model, LPCTSTR _filename)->bool{_model = GraphicsDevice.CreateModelFromFile(_filename); 
+	_model->SetScale(transform.scale); return model == nullptr ? false : true; };
+
+	ModelPrefarence(model,      _T("model3D//âºëfçﬁ//hashira_01.X"));
+	ModelPrefarence(model_road, _T("model3D//ìπòH//road_v01.X"));
 
 	const unsigned int model_size = 5;
 
-	for (int i = 1; i < model_size; ++i)
-	{
-		model_pos.push_back(Vector3(-2, 0, -(i * 20)));
-		model_pos.push_back(Vector3( 2, 0, -(i * 20)));
-	}
-
 	for (int i = 0; i < model_size; ++i)
 	{
-		model_road_pos.push_back(Vector3(0, 0, -(i * 10)));
+		if (i <= 3) {
+			model_pos.push_back(Vector3(-2, 0, -((i + 1) * 20)));
+			model_pos.push_back(Vector3( 2, 0, -((i + 1) * 20)));
+		}
+		model_road_pos.push_back(Vector3(0, 0, -(i * 12)));
 	}
-
 };
 void C_MAP:: Update()
 {
@@ -27,14 +25,13 @@ void C_MAP:: Update()
 };
 void C_MAP::Draw3D()
 {
+	auto&& ModelDrawPrefarence = [](MODEL& _model, Vector3& _pos) { _model->SetPosition(Vector3(_pos.x, _pos.y, _pos.z += 0.5f)); _model->Draw(); };
 
 	for (int i = 0; i < model_road_pos.size(); ++i)
 	{
-		if (model_road_pos[i].z >= 5.0f)
-			model_road_pos[i].z = -40.f;
+		if (model_road_pos[i].z >= 5.0f)model_road_pos[i].z = -45.f;
 
-		this->model_road->SetPosition(Vector3(model_road_pos[i].x, model_road_pos[i].y, model_road_pos[i].z += 0.5f));
-	    this->model_road->Draw();
+		ModelDrawPrefarence(model_road, model_road_pos[i]);
 	}
 
 
@@ -46,14 +43,12 @@ void C_MAP::Draw3D()
 
 		this->model->SetRotation(this->transform.rotation);
 
-		if (model_pos[i].z >= 5.0f)
-			model_pos[i].z = -70.f;
+		if (model_pos[i].z >= 5.0f)model_pos[i].z = -70.f;
 
-		this->model->SetPosition(Vector3(model_pos[i].x, model_pos[i].y, model_pos[i].z += 0.5f));
-	    this->model->Draw();
-
+		ModelDrawPrefarence(model, model_pos[i]);
 	}
 };
+
 void C_MAP::Draw2D() 
 {
 	
